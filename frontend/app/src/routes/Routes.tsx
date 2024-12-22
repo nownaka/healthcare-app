7ff4770b222b0df06e0e8987c2485862ee2fdc4b
カムイ <178002956+kamui72@users.noreply.github.com>
2024-12-22 19:07:30 +0900

    add:routes実装

--- frontend/app/src/routes/Routes.tsx
+++ frontend/app/src/routes/Routes.tsx
@@ -1,1 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import App from "../App";
+import Home from "../components/pages/HomePage";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/Home" element={<Home />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
