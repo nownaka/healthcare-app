0a1622c6e4c7f88543f379d3e1ccf853a5e25ca0
カムイ <178002956+kamui72@users.noreply.github.com>
2025-01-13 17:29:46 +0900

    Update:レイアウト調整

--- frontend/app/src/components/organisms/Header.tsx
+++ frontend/app/src/components/organisms/Header.tsx
@@ -1,30 +1,52 @@
 import styled from "styled-components";
-import SettingImage from "./setting.svg"
-
-const StyledHeader = styled.h1`
-    color: red;
-`
-const StyledImg = styled.img`
-    width: 20px;
-    height: auto;
-`
+import SettingImage from "./setting.svg";
 
 type Header = {
+  title: string;
+  userName: string;
+  textColor: string;
+};
 
-}
+const Header = (header: Header) => {
+  const { title, userName, textColor } = header;
 
-const Header = (text: Header) => { 
-    const title = "健康管理アプリ(仮)"
-    const userName = "山田 太郎"
+  /* style */
+  const StyledHeader = styled.h1`
+    color: ${textColor};
+  `;
+  const StyledUserName = styled.span`
+    font-weight: bold;
+    color: ${textColor};
+  `;
+  const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    background: #8cb33e;
+  `;
 
-    return (
-        <>
-            <StyledHeader>{title}</StyledHeader>
-            <StyledImg src={SettingImage} alt={"設定"} />
-            {userName}
-        </>
-    )
+  const StyledImg = styled.img`
+    width: 20px;
+    height: auto;
+    margin-right: 10px;
+  `;
 
-}
+  const UserSection = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: auto;
+  `;
 
-export default Header
+  return (
+    <HeaderContainer>
+      <StyledHeader>{title}</StyledHeader>
+      <UserSection>
+        <StyledImg src={SettingImage} alt={"設定"} />
+        <StyledUserName>{userName}</StyledUserName>
+      </UserSection>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
