54dc4bbb5765ffbc84363cc3a5044962f8ba9a08
カムイ <178002956+kamui72@users.noreply.github.com>
2025-01-13 17:34:24 +0900

    add:サインアップページルート追加

--- frontend/app/src/routes/Routes.tsx
+++ frontend/app/src/routes/Routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import App from "../App";
 import Home from "../components/pages/HomePage";
+import SignUpPage from "../components/pages/SignUpPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/Home" element={<Home />} />
+      <Route path='/' element={<App />} />
+      <Route path='/signup' element={<SignUpPage />} />
+      <Route path='/Home' element={<Home />} />
     </Routes>
   );
 };
