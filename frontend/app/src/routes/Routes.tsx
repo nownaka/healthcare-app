5a74e08187c4f8bcbb6e6bb536f628ca3236043c
カムイ <178002956+kamui72@users.noreply.github.com>
2025-02-17 01:13:16 +0900

    add:利用規約ページとプロフィール編集画面のルート追加

--- frontend/app/src/routes/Routes.tsx
+++ frontend/app/src/routes/Routes.tsx
@@ -2,13 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import App from "../App";
 import Home from "../components/pages/HomePage";
 import SignUpPage from "../components/pages/SignUpPage";
+import TermsPage from "../components/pages/TermsPage";
+import SettingsPage from "../components/pages/SettingsPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/signup' element={<SignUpPage />} />
-      <Route path='/Home' element={<Home />} />
+      <Route path="/" element={<App />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/Home" element={<Home />} />
+      <Route path="/Terms" element={<TermsPage />} />
+      <Route path="/Settings" element={<SettingsPage />} />
     </Routes>
   );
 };
