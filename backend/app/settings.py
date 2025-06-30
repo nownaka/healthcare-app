e6660de7fb2faf586dec590790620f575159df0b
nownaka <98019169+nownaka@users.noreply.github.com>
2025-06-30 16:03:11 +0900

    Fix: 環境変数 DJANGO_ALLOWED_HOSTS が反映されていないバグを修正

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -26,10 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [
-#     os.getenv('DJANGO_ALLOWED_HOSTS')
-# ]
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 CSRF_TRUSTED_ORIGINS = [
     os.getenv('DJANGO_CSRF_TRUSTED_ORIGINS')
@@ -151,7 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # 自身以外のオリジンのHTTPリクエスト内にクッキーを含めることを許可する
 CORS_ALLOW_CREDENTIALS = True
 # アクセスを許可したいURL（アクセス元）を追加
-CORS_ALLOWED_ORIGINS = os.environ.get("DJANGO_CORS_ALLOWED_ORIGINS").split(" ")
+CORS_ALLOWED_ORIGINS = os.environ.get("DJANGO_CORS_ALLOWED_ORIGINS").split(",")
 print(os.environ.get("DJANGO_CORS_ALLOWED_ORIGINS"))
 # プリフライト(事前リクエスト)の設定
 # 30分だけ許可
