2f37413674685b498fab02ea13b1dcda2a466ae7
nownaka <98019169+nownaka@users.noreply.github.com>
2025-08-05 22:57:07 +0900

    Test: バックエンドAPIのポート削除

--- frontend/app/src/config.ts
+++ frontend/app/src/config.ts
@@ -10,7 +10,7 @@ const checkEnvValue = (envName: string): string => {
 const backendApiProtocol = process.env.REACT_APP_BACKEND_API_PROTOCOL || "http";
 const backendAPIhostName =
   process.env.REACT_APP_BACKEND_API_HOST_NAME || "localhost";
-const backendAPIPort = process.env.REACT_APP_BACKEND_API_PORT || "8000";
+const backendAPIPort = ""; //process.env.REACT_APP_BACKEND_API_PORT || "8000";
 const backendAPIBaseUrl = `${backendApiProtocol}://${backendAPIhostName}:${backendAPIPort}`;
 
 export const config = {
